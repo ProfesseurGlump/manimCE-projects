cddefc76e7d990a86a327fcfeee2ddc49e69a38e
Laurent Garnier <professeur.glump@gmail.com>
2023-11-02 18:28:52 +0200

    add details for matrix multiplication

--- detail_multiplication.py
+++ detail_multiplication.py
@@ -77,12 +77,12 @@ class MultiplyMatrix(Scene):
         self.play(FadeIn(youtube_shorts.to_edge(2.5*UP)))
 
         
-        def disp_ABCdetails(A, B, C_col0, C_col1, C, switch):
-            abcdetails = Group(
-                A, B, C_col0, C_col1
+        def disp_ABC_details(A, B, C, C_details, switch):
+            abc_details = Group(
+                A, B, C, C_details
             ).arrange_in_grid(buff=1.25)
-            if switch == 1: self.play(FadeIn(abcdetails))
-            elif switch == -1: self.play(FadeOut(abcdetails))
+            if switch == 1: self.play(FadeIn(abc_details))
+            elif switch == -1: self.play(FadeOut(abc_details))
             self.wait(0.5)
         
 
@@ -101,26 +101,7 @@ class MultiplyMatrix(Scene):
 
             B_22 = Matrix(ent_B_22)
 
-            
-            c00 = "1\\times 4 + 3\\times 2"
-            c01 = "3\\times 4 + 4\\times 2"
-            c10 = "1\\times 3 + 3\\times 1"
-            c11 = "1\\times 4 + 3\\times 2"
-            
-            ent_C_col0 = [
-                [c00],
-                [c10]
-            ]
-
-            C_col0 = Matrix(ent_col0)
-            
-            ent_C_col1 = [
-                [c01],
-                [c11]
-            ]
-            
-            C_col1 = Matrix(ent_col1)
-            
+                        
             ent_AB_22 = [
                 [8, 5],
                 [20, 13]
@@ -128,29 +109,45 @@ class MultiplyMatrix(Scene):
 
             C_22 = Matrix(ent_AB_22)
 
-            d00 = MathTex("4\times 1 + 3\times 3")
-            d01 = MathTex("4\times 2 + 3\times 4")
-            d10 = MathTex("2\times 1 + 1\times 3")
-            d11 = MathTex("2\times 2 + 1\times 4")
-            
-            ent_details_BA = [
-                [d00, d01],
-                [d10, d11]
+            c00 = "1\\times 4 + 3\\times 2"
+            c01 = "3\\times 4 + 4\\times 2"
+            c10 = "1\\times 3 + 3\\times 1"
+            c11 = "1\\times 4 + 3\\times 2"
+
+            ent_C_details = [
+                [c00],
+                [c01],
+                [c10],
+                [c11]
             ]
             
-            D_22_details = Matrix(ent_details_BA)
+            C_details = Matrix(ent_C_details)
             
             ent_BA_22 = [
                 [13, 20],
                 [5, 8]
-                ]
+            ]
             
             D_22 = Matrix(ent_BA_22)
+            
+            d00 = "4\\times 1 + 3\\times 3"
+            d01 = "4\\times 2 + 3\\times 4"
+            d10 = "2\\times 1 + 1\\times 3"
+            d11 = "2\\times 2 + 1\\times 4"
+            
+            ent_D_details = [
+                [d00],
+                [d01],
+                [d10],
+                [d11]
+            ]
+            
+            D_details = Matrix(ent_D_details)
+            
+            return A_22, B_22, C_22, C_details, D_22, D_details
 
-            return A_22, B_22, C_col0, C_col1, C_22, D_22_details, D_22
-
-        A, B, C_col0, C_col1, C, D_details, D = test_values()
-        disp_ABCdetails(A, B, C, C_details, 1)
+        A, B, C, C_details, D, D_details = test_values()
+        disp_ABC_details(A, B, C, C_details, 1)
         
         # Lignes de A
         A_row0_fix = SurroundingRectangle(A.get_rows()[0])
@@ -172,16 +169,6 @@ class MultiplyMatrix(Scene):
 
             
         # Éléments du produit AB
-        ent_C_col0 = C_col0.get_entries()
-        C_col00_fix = SurroundingRectangle(ent_C_col0[0])
-        C_col10_fix = SurroundingRectangle(ent_C_col0[1])
-        C_col0_elts_fix = [C_col00_fix, C_col10_fix]
-
-        ent_C_col1 = C_col1.get_entries()
-        C_col01_fix = SurroundingRectangle(ent_C_col1[0])
-        C_col11_fix = SurroundingRectangle(ent_C_col1[1])
-        C_col1_elts_fix = [C_col01_fix, C_col11_fix]
-
         ent_C = C.get_entries()
         C_00_fix = SurroundingRectangle(ent_C[0])
         C_01_fix = SurroundingRectangle(ent_C[1])
@@ -192,95 +179,170 @@ class MultiplyMatrix(Scene):
             C_10_fix, C_11_fix
         ]
 
+        ent_C_details = C_details.get_entries()
+        C_details_00_fix = SurroundingRectangle(ent_C_details[0])
+        C_details_01_fix = SurroundingRectangle(ent_C_details[1])
+        C_details_10_fix = SurroundingRectangle(ent_C_details[2])
+        C_details_11_fix = SurroundingRectangle(ent_C_details[3])
+        C_details_elts_fix = [
+            C_details_00_fix, C_details_01_fix,
+            C_details_10_fix, C_details_11_fix
+        ]
+
+        C_details_00_move = SurroundingRectangle(ent_C_details[0])
+        C_details_01_move = SurroundingRectangle(ent_C_details[1])
+        C_details_10_move = SurroundingRectangle(ent_C_details[2])
+        C_details_11_move = SurroundingRectangle(ent_C_details[3])
+        C_details_elts_move = [
+            C_details_00_move, C_details_01_move,
+            C_details_10_move, C_details_11_move
+        ]
+        
         #####################
         # Ligne 1 colonne 1 #
         #####################
 
-        # STEP 1: Encadre A[0:], B[:0] et C[0:0]
+        # STEP 1: Encadre A[0:], B[:0]
         self.play(
             Write(A_row_fix[0]),
             Write(B_col_fix[0]),
-            Write(C_details_elts_fix[0]),
         )
-        self.wait(0.45)
+        self.wait(2.5)
 
-        # STEP 2: Envoie les cadres A[0:] et B[:0] sur C_details[0:0]
+        # STEP 2: Envoie les cadres
+        # 1) A[0:] et B[:0] sur C_details[0]
         self.play(
             ReplacementTransform(A_row_move[0], C_details_elts_fix[0]),
             ReplacementTransform(B_col_move[0], C_details_elts_fix[0])
         )
-        self.wait(0.3)
+        self.wait(2.5)
         A_row_move[0] = SurroundingRectangle(A.get_rows()[0])
         B_col_move[0] = SurroundingRectangle(B.get_columns()[0])
 
         # STEP 3: Déplace
-        # 1) B[:0] vers B[:1]
-        # 2) C_details[0:0] vers C_details[0:1]
+        # 1) C_details[0] sur C[0:0]
         self.play(
-            ReplacementTransform(B_col_fix[0], B_col_fix[1]),
-            ReplacementTransform(C_details_elts_fix[0], C_details_elts_fix[1])
+            ReplacementTransform(C_details_elts_move[0], C_elts_fix[0]),
         )
-        self.wait(0.3)
-        B_col_fix[0] = SurroundingRectangle(B.get_columns()[0])
+        self.wait(2.5)
+        C_details_elts_move[0] = SurroundingRectangle(ent_C_details[0])
         
 
         #####################
         # Ligne 1 colonne 2 #
         #####################
+
+        # STEP 4: Déplace
+        # 1) B[0:] vers B[1:]
+        # 2) C_details[0] vers C_details[1]
+        # 3) C[0:0] vers C[0:1]
+        self.play(
+            ReplacementTransform(B_col_fix[0], B_col_fix[1]),
+            ReplacementTransform(
+                C_details_elts_fix[0],
+                C_details_elts_fix[1]
+            ),
+            ReplacementTransform(C_elts_fix[0], C_elts_fix[1])
+        )
+        self.wait(2.5)
+        B_col_fix[0] = SurroundingRectangle(B.get_columns()[0])
         
-        # STEP 4: Envoie les cadres A[0:] et B[:1] sur C_details[0:1]
+        # STEP 5: Envoie les cadres
+        # 1) A[0:] et B[:1] sur C_details[1]
         self.play(
             ReplacementTransform(A_row_move[0], C_details_elts_fix[1]),
             ReplacementTransform(B_col_move[1], C_details_elts_fix[1])
         )
-        self.wait(0.3)
+        self.wait(2.5)
         A_row_move[0] = SurroundingRectangle(A.get_rows()[0])
         B_col_move[1] = SurroundingRectangle(B.get_columns()[1])
 
         # STEP 5: Déplace
-        # 1) A[0:] vers A[1:]
-        # 2) B[:1] vers B[:0]
-        # 3) C_details[0:1] vers C_details[1:0]
+        # 1) C_details[1] vers C[0:1]
+        self.play(
+            ReplacementTransform(C_details_elts_move[1], C_elts_fix[1]),
+        )
+        self.wait(2.5)
+
+        # STEP 6:
+        # 2) A[0:] vers A[1:]
+        # 3) B[:1] vers B[:0]
+        # 4) C[0:1] vers C[1:0]
         self.play(
             ReplacementTransform(A_row_fix[0], A_row_fix[1]),
             ReplacementTransform(B_col_fix[1], B_col_fix[0]),
-            ReplacementTransform(C_details_elts_fix[0], C_details_elts_fix[2]),
-            ReplacementTransform(C_details_elts_fix[1], C_details_elts_fix[2]),
+            ReplacementTransform(
+                C_details_elts_fix[0],
+                C_details_elts_fix[2]
+            ),
+            ReplacementTransform(
+                C_details_elts_fix[1],
+                C_details_elts_fix[2]
+            ),
+            ReplacementTransform(C_elts_fix[0], C_elts_fix[2]),
+            ReplacementTransform(C_elts_fix[1], C_elts_fix[2]),
         )
-        self.wait(0.3)
-        A_row_fix[0] = SurroundingRectangle(A.get_rows()[0])
+        self.wait(2.5)
         B_col_fix[1] = SurroundingRectangle(B.get_columns()[1])
         
-        # STEP 6: envoie les cadres A[:1] et B[:0] sur C_details[1:0]
+        # STEP 7: envoie les cadres
+        # 1) A[:1] et B[:0] sur C_details[2]
         self.play(
             ReplacementTransform(A_row_move[1], C_details_elts_fix[2]),
             ReplacementTransform(B_col_move[0], C_details_elts_fix[2]),
         )
-        self.wait(0.3)
+        self.wait(0.9)
         A_row_move[1] = SurroundingRectangle(A.get_rows()[1])
         B_col_move[0] = SurroundingRectangle(B.get_columns()[0])
 
-        # STEP 7: Déplace
+        # STEP 8: Envoie
+        # 1) C_details[2] sur C[1:0]
+        self.play(
+            ReplacementTransform(C_details_elts_move[2], C_elts_fix[2]),
+        )
+        self.wait(2.5)
+
+        # STEP 9: Déplace
         # 1) B[:0] vers B[:1]
-        # 2) C_details[1:0] vers C_details[1:1]
+        # 2) C_details[2] vers C_details[3]
+        # 2) C[1:0] vers C[1:1]
         self.play(
             ReplacementTransform(B_col_fix[0], B_col_fix[1]),
-            ReplacementTransform(C_details_elts_fix[2], C_details_elts_fix[3]),
-            ReplacementTransform(C_details_elts_fix[1], C_details_elts_fix[3]),
-            ReplacementTransform(C_details_elts_fix[0], C_details_elts_fix[3])
+            ReplacementTransform(
+                C_details_elts_fix[0],
+                C_details_elts_fix[3]
+            ),
+            ReplacementTransform(
+                C_details_elts_fix[1],
+                C_details_elts_fix[3]
+            ),
+            ReplacementTransform(
+                C_details_elts_fix[2],
+                C_details_elts_fix[3]
+            ),
+            ReplacementTransform(C_elts_fix[0], C_elts_fix[3]),
+            ReplacementTransform(C_elts_fix[1], C_elts_fix[3]),
+            ReplacementTransform(C_elts_fix[2], C_elts_fix[3])
         )
-        self.wait(0.3)
-        B_col_fix[0] = SurroundingRectangle(B.get_columns()[0])
-
-        # STEP 8: envoie les cadres A[:1] et B[:1] sur C_details[1:1]
+        self.wait(2.5)
+        
+        # STEP 10: envoie les cadres A[:1] et B[:1] sur C_details[3]
         self.play(
             ReplacementTransform(A_row_move[1], C_details_elts_fix[3]),
             ReplacementTransform(B_col_move[1], C_details_elts_fix[3]),
         )
-        self.wait(0.3)
+        self.wait(2.5)
         A_row_move[1] = SurroundingRectangle(A.get_rows()[1])
         B_col_move[1] = SurroundingRectangle(B.get_columns()[1])
-        
+
+        # STEP 11: envoie C_details[3] sur C[1:1]
+        self.play(
+            ReplacementTransform(
+                C_details_elts_move[3],
+                C_elts_fix[3]
+            ),
+        )
+        self.wait(2.5)
 
         disp_sub(self, lang='fr')
 
